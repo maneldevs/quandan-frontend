8a1aadb13b35dd5601e7ac7c09cc8d2defd67b44
maneldevs <maneldevs@gmail.com>
2023-08-16 19:57:55 +0200

    add vuetify and mdi icons

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import '@mdi/font/css/materialdesignicons.css'
+import { aliases, mdi } from 'vuetify/lib/iconsets/mdi'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
+
+export default vuetify
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <main>
     <h1>Bienvenido</h1>
+    <v-icon icon="mdi:mdi-home" />
   </main>
 </template>
